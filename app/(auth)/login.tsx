4fd43c87d2d81a34ca288d33091e22cbd5b1da02
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-06-30 21:28:48 +0100

    Autentication nav route. Imstalled dependencies.

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
